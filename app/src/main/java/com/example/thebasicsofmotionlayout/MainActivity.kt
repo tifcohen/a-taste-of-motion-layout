cab4df72397ba6d0d6edab8f69a5019a25609875
tiferet_cohen <tiferet@zemingo.com>
2020-05-23 20:07:53 +0300

    change title motion sizing fade in / out for btn label shrink for btn label

--- app/src/main/java/com/example/thebasicsofmotionlayout/MainActivity.kt
+++ app/src/main/java/com/example/thebasicsofmotionlayout/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.thebasicsofmotionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,6 +21,33 @@ class MainActivity : AppCompatActivity() {
         scrollable_content.apply {
             adapter = viewAdapter
         }
+
+
+        motion_layout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+
+            }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+
+            }
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+                (1 - progress).also {
+                    label1.run {
+//                        alpha = it
+                        scaleX = it
+                        scaleY = it
+                    }
+                    label2.alpha = it
+                    label3.alpha = it
+                }
+            }
+        })
     }
 
     private fun createList(): List<String> {
