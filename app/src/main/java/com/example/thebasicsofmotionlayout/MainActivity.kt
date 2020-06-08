7f083213f95cc3930a9606537ad8b823488d52d4
tiferet_cohen <tiferet@zemingo.com>
2020-06-08 22:34:42 +0300

    final changes

--- app/src/main/java/com/example/thebasicsofmotionlayout/MainActivity.kt
+++ app/src/main/java/com/example/thebasicsofmotionlayout/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.thebasicsofmotionlayout
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.motion.widget.MotionLayout
@@ -26,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         motion_layout.setTransitionListener(object : MotionLayout.TransitionListener {
             override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-
+//                Toast.makeText(this@MainActivity, "onTransitionCompleted", Toast.LENGTH_SHORT).show()
             }
 
             override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
@@ -34,11 +35,11 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
-
+//                Toast.makeText(this@MainActivity, "onTransitionStarted", Toast.LENGTH_SHORT).show()
             }
 
             override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
-
+                progress_display.text = "${(progress*100).toInt()}%"
             }
         })
     }
